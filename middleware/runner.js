561135c499a608e4ceb7eac88395861967a9bd8c
tgly307 <tgly307@163.com>
2018-06-28 02:57:59 +0800

    fix

--- middleware/runner.js
+++ middleware/runner.js
@@ -3,8 +3,13 @@ const { execSync } = require('child_process');
 module.exports = async (ctx, next) => {
     await next();
     if(ctx.state.command) {
-        const result = execSync(ctx.state.command).toString();
-        ctx.body = {data: result};
+        try{
+            const result = execSync(ctx.state.command).toString();
+            ctx.body = {data: result};
+        } catch (e) {
+            ctx.body = e.stderr.toString();
+        }
+
     }
 
 };
