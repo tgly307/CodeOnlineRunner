4f5bb667f72d05b95b62298dd74263504535e181
freemagico <tgly307@163.com>
2018-08-09 17:43:11 +0800

    test

--- middleware/handlepostdata.js
+++ middleware/handlepostdata.js
@@ -4,8 +4,11 @@ const path = require('path');
 const apiReg = /^\/api\//;
 
 module.exports = async (ctx, next) => {
-    ctx.state.codeFile = path.join(process.cwd(),'postCode',utils.uuid(20));
-    ctx.state.inputFile = path.join(process.cwd(),'postCode',utils.uuid(25));
+    ctx.state.codeFileName = utils.uuid(20);
+    ctx.state.inputFileName = utils.uuid(25);
+    ctx.state.filePath = path.join(process.cwd(), 'postCode')
+    ctx.state.codeFile = path.join(ctx.state.file, ctx.state.codeFileName);
+    ctx.state.inputFile = path.join(ctx.state.file, 'postCode', ctx.state.inputFileName);
     if (apiReg.test(ctx.path)) {
         if (ctx.request.body.code) {
             // save code file
--- routes/python.js
+++ routes/python.js
@@ -1,8 +1,10 @@
 module.exports = async (ctx) => {
     ctx.state.command = 'docker run --rm ' +
-                        '-v $PWD:/usr/src/myapp  ' +
+                        '-v ' +
+                        ctx.state.filePath +
+                        ':/usr/src/myapp  ' +
                         '-w /usr/src/myapp python:alpine timeout python ' +
-                        ctx.state.codeFile;
+                        ctx.state.codeFileName;
                         // + ' 2>&1';
     console.log(ctx.state.command)
 };
