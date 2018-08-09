a3ef48cb9a8051e2f927e3ce5cf31bb99b02342f
freemagico <tgly307@163.com>
2018-08-09 17:47:03 +0800

    test

--- middleware/handlepostdata.js
+++ middleware/handlepostdata.js
@@ -7,8 +7,8 @@ module.exports = async (ctx, next) => {
     ctx.state.codeFileName = utils.uuid(20);
     ctx.state.inputFileName = utils.uuid(25);
     ctx.state.filePath = path.join(process.cwd(), 'postCode')
-    ctx.state.codeFile = path.join(ctx.state.file, ctx.state.codeFileName);
-    ctx.state.inputFile = path.join(ctx.state.file, 'postCode', ctx.state.inputFileName);
+    ctx.state.codeFile = path.join(ctx.state.filePath, ctx.state.codeFileName);
+    ctx.state.inputFile = path.join(ctx.state.filePath, 'postCode', ctx.state.inputFileName);
     if (apiReg.test(ctx.path)) {
         if (ctx.request.body.code) {
             // save code file
