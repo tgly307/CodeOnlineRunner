4f22bb78295ef7d6e24a2e5658326b6677e78f70
freemagico <tgly307@163.com>
2018-08-09 17:17:15 +0800

    fix a huge bug

--- routes/nodejs.js
+++ routes/nodejs.js
@@ -2,7 +2,7 @@ module.exports = async (ctx) => {
     // ctx.state.command = 'node ' + ctx.state.codeFile;
     ctx.state.command = 'docker run --rm ' +
         '-v $PWD:/usr/src/myapp  ' +
-        '-w /usr/src/myapp node:alpine &&timeout 5 node ' +
+        '-w /usr/src/myapp node:alpine timeout node ' +
         ctx.state.codeFile;
         //+ ' 2>&1';
 };
